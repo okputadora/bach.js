9ad522aa3e500bac2a8541f34c6b76e4ba56f7bf
okputadora <okputadora@gmail.com>
2018-10-28 20:54:35 -0400

    successfully build ngrams from ALL files

--- parse.js
+++ parse.js
@@ -4,6 +4,7 @@ const distance = require('tonal-distance');
 const Ngram = require('./Models/Ngram');
 const mongoose = require('mongoose');
 const KEYS = {
+  '0': {major: 'C', minor: 'A'},
   '1': {major: 'G', minor: 'E'},
   '2': {major: 'D', minor: 'B'},
   '3': {major: 'A', minor: 'F#'},
@@ -25,11 +26,22 @@ const DURATIONS = {
   'half': 'x-------',
   'whole': 'x---------------',
 }
+mongoose.connect('mongodb://localhost/classicalMusic', async (err, res) => {
+  if (err){console.log('DB CONNECTION FAILED: '+err)}
+  else{console.log('DB CONNECTION SUCCESS')}
+  getPaths('./unzippedScores')
+  .then(files => batchParse(files))
+  .then(parsedSongs => {
+    console.log('songs are parsed, we parsed ', parsedSongs.length)
+    // console.log(parsedSongs[3])
+    getNgramsFromSongs(parsedSongs, 4, 0)
+  }) // 4 = ngram size 0 = index to start from of parsedSongs
 
-getPaths('./unzippedScores')
-.then(res => batchParse(res))
+})
+
 // .then(res => console.log(res))
 
+//  ITHINK THIS CAN BE COMBINED WITH GETPATH
 function getPaths(directory) { // unzippedScored
   return new Promise ((resolve, reject) => {
     fs.readdir(directory, function(err, res){
@@ -59,111 +71,161 @@ function getPath(directory, srcPaths, index, paths) {
   })
 }
 
+
 function batchParse(files) {
-  // console.log(files[0])
-  fs.readFile(files[0], 'utf8', function (err, res) {
-    // console.log(res)
-    music.musicJSON(res, function (err, json) {
-      // Do something with the MusicJSON data
-      if (err) console.log(err)
-      // console.log(json['score-partwise']['part'][0]['measure'][0].note)
-      let musicJson = {
-        key: '',
-        timeSignature: '',
-        notes: [],
-      
-      }
-      // console.log(JSON.stringify(json, null, 2))
-      json['score-partwise']['part'][0]['measure'].forEach(function(measure, i){
-        // console.log(measure['$'].number)
-        if (measure['$'].number === '1') {
-          musicJson.key = KEYS[measure.attributes.key.fifths].major
-          // console.log(measure)
-          musicJson.timeSignature = `${measure.attributes.time.beats}/${measure.attributes.time['beat-type']}`
-          // console.log(measure.note[0])
-          let end = 0;
-        }
-        // console.log("TEST JSON: ", testJson)
-        // console.log(measure)
-        if (measure.note.length > 0) {
-          measure.note.forEach(function(note, x){
-            // console.log("NOTE", note)
-            if (note.pitch) {
-              let noteName = `${note.pitch.step}${note.pitch.octave}`
-              musicJson.notes.push({
-                noteName: noteName, 
-                duration: note.type,
+  console.log('parsing files')
+  return new Promise((resolve, reject) => {
+    Promise.all(files.map((file, i) => {
+      return new Promise((resolve, reject) => {
+        fs.readFile(file, 'utf8', function (err, res) {
+          // console.log(res)
+          music.musicJSON(res, function (err, json) {
+            // Do something with the MusicJSON data
+            if (err) console.log(err)
+            // console.log(json['score-partwise']['part'][0]['measure'][0].note)
+            try {
+              let musicJson = {
+                key: '',
+                timeSignature: '',
+                notes: [],
+              
+              }
+              // console.log(JSON.stringify(json, null, 2))
+              json['score-partwise']['part'][0]['measure'].forEach(function(measure, x){
+                if (i === 1 && x === 0) console.log("MEASRE: ",measure)
+                // console.log(measure['$'].number)
+                if (measure['$'].number === '1' && measure.attributes && measure.attributes.key.fifths) {
+                  musicJson.key = KEYS[measure.attributes.key.fifths].major // NEED TO DECIDE IF ITS MINOR OR MAJOR
+                  console.log("KEY: ", musicJson.key)
+                  // console.log(measure)
+                  musicJson.timeSignature = `${measure.attributes.time.beats}/${measure.attributes.time['beat-type']}`
+                  // console.log(measure.note[0])
+                  let end = 0;
+                }
+                // console.log("TEST JSON: ", testJson)
+                // console.log(measure)
+                if (measure.note.length > 0) {
+                  measure.note.forEach(function(note, x){
+                    // console.log("NOTE", note)
+                    if (note.pitch) {
+                      let noteName = `${note.pitch.step}${note.pitch.octave}`
+                      musicJson.notes.push({
+                        noteName: noteName, 
+                        duration: note.type,
+                      })
+                    }
+                  })
+                }
               })
+              if (!musicJson.key) {
+                resolve(null)
+              } else {
+                console.log('successfully parsed ', i)
+                resolve(getIntervals(musicJson))
+              }
             }
+            catch (err) {
+              // console.log("ERROR: ",err);
+              resolve(null)
+            }
+            // console.log(musicJson)
+            // console.log(notes)
+
           })
-        }
+        })
+
       })
-      // console.log(notes)
-      getIntervals(musicJson)
+    }))
+    .then(parsedSongs => {
+      // console.log("success")
+      // console.log(parsedSongs)
+      return resolve(parsedSongs.filter(song => ((song != null) && song.key)))
     })
+    .catch(err => console.log("ERORR: ", err))
   })
 }
 
 function getIntervals(musicJson){
+  tonalKey = musicJson.key.replace('FLAT', 'b')
   musicJson.notes.forEach((note, i, arr) => {
+    noteName = note.noteName.replace('FLAT', 'b')
     // console.log(i)
-    note.relativeNote = distance.interval(`${musicJson.key}4`, note.noteName);
-    note.distanceFromPrev = i > 0 ? distance.semitones(arr[i - 1].noteName, note.noteName) : null;
+    note.relativeNote = distance.interval(`${tonalKey}4`, noteName); // THIS 4 SHOULD NOT BE HARDCODED WE NEED TO DEDUCE IT FROM music.Json.key
+    note.distanceFromPrev = i > 0 ? distance.semitones(arr[i - 1].noteName, noteName) : null;
   })
+  return musicJson
   // console.log(musicJson)
-  analyze(musicJson, 4)
 }
 
-// THIS NEEDS TO BE REFACTORED
-function analyze(musicJson, nGramSize) {
-  mongoose.connect('mongodb://localhost/classicalMusic', async (err, res) => {
-    if (err){console.log('DB CONNECTION FAILED: '+err)}
-    else{console.log('DB CONNECTION SUCCESS')}
-    let existingGrams = await Ngram.findOne({artist: 'Bach'})
-    console.log("EXISITING GRAMS: ", existingGrams)
-    let nGramData = {};
-    musicJson.notes.forEach((note, i, arr) => {
-      let target = '';
-      let nextChunk = '';
-      if (i + 8 > arr.length) return;
-      // Get four note chunk
-      for (let x = 0; x < nGramSize; x++) {
-        target += `${arr[i + x].relativeNote}@${arr[i + x].duration}&`
-        nextChunk += `${arr[i + x + nGramSize].relativeNote}@${arr[i + x + nGramSize].duration}&`
-      }
-      // Check if this four note chunk exists in the db
-      // THIS NEEDS TO BE REFACTORED ITS WEIRD WE HAVE BOTH nGramData and existingNGrams
-      if (existingGrams) {
-        if (existingGrams.nGrams[target]) {
-          console.log("FOUND AN EXISTING ONE: ", existingGrams.nGrams[target])
-          if (existingGrams.nGrams[target][nextChunk]) {
-            existingGrams.nGrams[target][nextChunk] += 1 
-          } else existingGrams.nGrams[target][nextChunk] = 1
-        } 
-        else {
-          existingGrams.nGrams[target] = {[nextChunk]: 1};
-        } 
-      } 
-      else if (nGramData[target]) {
-        if (nGramData[target][nextChunk]) {
-          nGramData[target][nextChunk] += 1;
-        } else nGramData[target][nextChunk = 1];
-      } 
-      else nGramData[target] = {[nextChunk]: 1};
-    })
-    // console.log(nGramData);
-    if (!existingGrams) {
-      // await existingGrams.save()
-      Ngram.create({artist: 'Bach', nGrams: nGramData})
-      .then(res => {
-        console.log('success')
-        mongoose.connection.close();
-      })
-    } else {
-      await existingGrams.save()
-      mongoose.connection.close();
+async function getNgramsFromSongs(songs, nGramSize, index) {
+  if (index === 0 ){
+
+     console.log(songs.map(song => song.key))
+  }
+  // console.log('current song: ', index)
+  let existingGrams = await Ngram.findOne({artist: 'Bach'})
+  let nGramData = {};
+  let song = songs[index]
+  if (song == null) {
+    index += 1;
+    console.log('couldnt parse song ', index)
+    return getNgramsFromSongs(songs, nGramSize, index)
+  }
+  song.notes.forEach((note, i, arr) => {
+    let target = '';
+    let nextChunk = '';
+    if (i + (nGramSize * 2) > arr.length) return;
+    // Get four note chunk
+    for (let x = 0; x < nGramSize; x++) {
+      target += `${arr[i + x].relativeNote}@${arr[i + x].duration}&`
+      nextChunk += `${arr[i + x + nGramSize].relativeNote}@${arr[i + x + nGramSize].duration}&`
     }
+    // Check if this four note chunk exists in the db
+    // THIS NEEDS TO BE REFACTORED ITS WEIRD WE HAVE BOTH nGramData and existingNGrams
+    if (existingGrams) {
+      if (existingGrams.nGrams[target]) {
+        console.log('the target exists')
+        if (existingGrams.nGrams[target][nextChunk]) {
+          existingGrams.nGrams[target][nextChunk] += 1 
+        } else existingGrams.nGrams[target][nextChunk] = 1
+      } 
+      else {
+        console.log('new targte')
+        existingGrams.nGrams[target] = {[nextChunk]: 1};
+      } 
+    } 
+    else if (nGramData[target]) {
+      if (nGramData[target][nextChunk]) {
+        nGramData[target][nextChunk] += 1;
+      } else nGramData[target][nextChunk] = 1;
+    } 
+    else nGramData[target] = {[nextChunk]: 1};
   })
- 
+  // console.log('successfully built n grams ')
+  index += 1;;
+  // console.log(nGramData);
+  if (!existingGrams) {
+    // await existingGrams.save()
+    console.log('NEW to db')
+    Ngram.create({artist: 'Bach', nGrams: nGramData})
+    .then(res => {
+      console.log('analyzing song no: ', index)
+      getNgramsFromSongs(songs, nGramSize, index)
+    })
+  } else {
+    console.log('writing to db')
+    await existingGrams.save()
+    if (index < songs.length) {
+      console.log('analyzing song no: ', index)
+      getNgramsFromSongs(songs, nGramSize, index)
+    }
+    else done()
+  }
 }
+// THIS NEEDS TO BE REFACTORED
+function done() {
+  console.log('success!')
+  mongoose.connection.close();
+}
+  
 
